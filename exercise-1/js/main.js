92fca51ad80390c63027fc7263bc9463064549e0
mkfreeman <michael.k.freeman@gmail.com>
2016-03-20 00:28:33 -0600

    Removed answers from exercises

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -8,25 +8,16 @@ $(function() {
   // Search songs function
   var searchSongs = function() {
       // Empty the images div
-      $('#image-div').empty();
+
 
       // Get the value from the input box
-      var song = $('input').val();
+
 
       // Make an AXAX request to the `songURL` + value from the input
-      $.ajax({
-        url:songURL + song,
-        type:'get'
-      }).then(function(data){
+
 
         // For each item that is returned, append an image with the imageURL associated with the songURL
-        data.tracks.items.forEach(function(d){
-          var img = $('<img></img>')
-          img.addClass('col-sm-3')
-          img.attr('src', d.album.images[0].url)
-          $('#image-div').append(img)
-        });
-      });
+        
 
   };
 
