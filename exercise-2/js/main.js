924f1209c82c31cf9848df8461727802b6660f53
quinnmau <mauq@uw.edu>
2016-04-04 22:17:26 -0700

    finished

--- exercise-2/js/main.js
+++ exercise-2/js/main.js
@@ -1,9 +1,31 @@
 // Main JavaScript File
+'use strict';
 
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
   // Use d3.csv to read in your `data/students.csv` dataset: remember, you must be running a local server
-
+    d3.csv('data/students.csv', function(error, data) {
+        console.log(data);
+        var table = $('<table></table>');
+        table.addClass('table');
+        
+        Object.keys(data[0]).forEach(function(d) {
+            var header = $('<th>' + d + '</th>');
+            table.append(header);
+        });
+        
+        data.forEach(function(d) {
+            var row = $('<tr>');
+            
+            Object.keys(d).forEach(function(dd) {
+                row.append($('<td>' + d[dd] + '</td>'));
+            });
+            
+            table.append(row);
+        });
+        
+        $('#sandbox').append(table);
+    });
     // Use jQuery to create a table, and store it in a variable
 
 
@@ -16,6 +38,4 @@ $(function() {
 
 
     // Select your `sandbox` section, and append your table to it
-    
-  });
 });
