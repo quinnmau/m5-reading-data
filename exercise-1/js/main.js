a57e8896aefeb63136d4ef8c1e424c20eb5b0e45
quinnmau <mauq@uw.edu>
2016-04-04 21:40:42 -0700

    finished

--- exercise-1/js/main.js
+++ exercise-1/js/main.js
@@ -1,4 +1,5 @@
 // Main JavaScript File
+'use strict';
 
 // You'll have to wait for you page to load to assign events to the elements created in your index.html file
 $(function() {
@@ -8,17 +9,24 @@ $(function() {
   // Search songs function
   var searchSongs = function() {
       // Empty the images div
-
+      $('#image-div').empty();
 
       // Get the value from the input box
-
+      var value = $('input[name=search]').val();
 
       // Make an AXAX request to the `songURL` + value from the input
-
-
-        // For each item that is returned, append an image with the imageURL associated with the songURL
-        
-
+      $.ajax({
+          url: songURL + value,
+          type: 'GET'
+      }).then (function(data) {
+          
+          data.tracks.items.forEach(function(d) {
+              var img = $('<img></img>');
+              img.addClass('col-sm-3');
+              img.attr('src', d.album.images[0].url);
+              $('#image-div').append(img);
+          });
+      });
   };
 
   // Assign a click event to your button with id `search-btn` to execute your `searchSongs` function
